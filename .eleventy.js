d8d3438157d499527841186a6ef2f66d29e3f3d8
Rares Portan <rares.portan@3pillarglobal.com>
2021-02-21 10:29:39 +0200

    First commit

---
+++ .eleventy.js
@@ -0,0 +1,48 @@
+const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
+// Filters
+const dateFilter = require('./src/filters/date-filter.js');
+const w3DateFilter = require('./src/filters/w3-date-filter.js');
+
+
+module.exports = config => {
+    config.addPassthroughCopy('./src/images/');
+
+    // Returns work items, sorted by display order
+    config.addCollection('work', collection => {
+        return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
+    });
+
+    // Returns work items, sorted by display order then filtered by featured
+    config.addCollection('featuredWork', collection => {
+        return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md')).filter(
+            x => x.data.featured
+        );
+    });
+
+    // Returns a collection of blog posts in reverse date order
+    config.addCollection('blog', collection => {
+        return [...collection.getFilteredByGlob('./src/posts/*.md')].reverse();
+    });
+
+    // Add filters
+    config.addFilter('dateFilter', dateFilter);
+    config.addFilter('w3DateFilter', w3DateFilter);
+
+    // Returns a list of people ordered by filename
+    config.addCollection('people', collection => {
+        return collection.getFilteredByGlob('./src/people/*.md').sort((a, b) => {
+            return Number(a.fileSlug) > Number(b.fileSlug) ? 1 : -1;
+        });
+    });
+
+
+    return {
+        markdownTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk', // this means we can use .html files instead of .njk files.
+        dir: {
+            input: 'src',
+            output: 'dist'
+        }
+    };
+};
